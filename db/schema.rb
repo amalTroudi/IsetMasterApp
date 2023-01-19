c686491240da2c9983642f9a1c380ac31b38d932
mohamedaminetr <mohamedamine.trimech@gmail.com>
2023-01-19 15:44:01 +0100

    fixes email confirmation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_17_104507) do
+ActiveRecord::Schema.define(version: 2023_01_19_085125) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,8 @@ ActiveRecord::Schema.define(version: 2023_01_17_104507) do
     t.integer "balance", default: 20
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "email_confirmed"
+    t.string "confirm_token"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
